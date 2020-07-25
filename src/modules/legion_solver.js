74f92926a46555fc70c11caf47ff1d54edd216e4
Rasa Rahnema <rasarahnema@yahoo.ca>
2020-07-25 00:21:21 -0400

    Initial Legion Solver

---
+++ src/modules/legion_solver.js
@@ -0,0 +1,139 @@
+import { Point } from './point.js';
+
+class LegionSolver {
+    constructor(board, pieces, pieceUpdated, a) {
+        this.board = board;
+        this.pieces = pieces;
+        this.pieceUpdated = pieceUpdated;
+        this.a = a;
+
+        this.middle = [];
+        for (let i = 9; i < 11; i++) {
+            for (let j = 10; j < 12; j++) {
+                if (this.board[i][j] != -1) {
+                    this.middle.push(new Point(i, j));
+                }
+            }
+        }
+    }
+
+    async solve() {
+        this.pieces.sort((a, b) => b.amount - a.amount);
+        return await this.solveInternal(0);
+    }
+
+    async solveInternal(position) {
+        this.a++;
+        console.log(this.a);
+
+        await new Promise(resolve => setTimeout(resolve, 0));
+        if (this.pieces.length > 0 && this.pieces[0].amount == 0) {
+            return true;
+        }
+        let point = new Point(position % this.board[0].length, Math.floor(position / this.board[0].length));
+    
+        if (point.y >= this.board.length) {
+            return true;
+        }
+    
+        if (this.board[point.y][point.x] != 0) {
+            return await this.solveInternal(position + 1);
+        }
+    
+        for (let k = 0; k < this.pieces.length; k++) {
+            for (let piece of this.pieces[k].transformations) {
+                if (this.pieces[k].amount == 0) {
+                    return false;
+                }
+                if (this.isPlaceable(point, piece)) {
+                    this.placePiece(point, piece);
+                    this.pieceUpdated(point, piece, true);
+                    let spotsMoved = this.takeFromList(k);
+                    if (this.isValid() && await this.solveInternal(position + 1)) {
+                        return true;
+                    }
+                    this.returnToList(k, spotsMoved);
+                    this.takeBackPiece(point, piece);
+                    this.pieceUpdated(point, piece, false);
+                }
+            }
+        }
+        return false;
+    }
+
+    takeFromList(placement) {
+        this.pieces[placement].amount--;
+        if (placement == this.pieces.length - 1) {
+            return 0;
+        }
+    
+        let fill = this.pieces[placement];
+        if (this.pieces[placement].amount >= this.pieces[placement + 1].amount) {
+            return 0;
+        } else {
+            for (let i = placement + 1; i < this.pieces.length; i++) {
+                if (this.pieces[placement].amount >= this.pieces[i].amount) {
+                    this.pieces[placement] = this.pieces[i - 1];
+                    this.pieces[i - 1] = fill;
+                    return i - 1 - placement;
+                }
+            }
+        }
+    
+        this.pieces[placement] = this.pieces[this.pieces.length - 1];
+        this.pieces[this.pieces.length - 1] = fill;
+        return this.pieces.length - 1 - placement;
+    }
+
+    returnToList(placement, spotsMoved) {
+        let fill = this.pieces[placement];
+        this.pieces[placement] = this.pieces[placement + spotsMoved];
+        this.pieces[placement + spotsMoved] = fill;
+        this.pieces[placement].amount++;
+    }
+
+    isValid() {
+        if (this.middle.length == 0) 
+            return true;
+        
+        let normalPieces = 0;
+        for (let point of this.middle) {
+            if (this.board[point.x][point.y] > 0 && this.board[point.x][point.y] <= this.pieces.length) {
+                normalPieces++;
+            }
+        }
+    
+        return normalPieces != this.middle.length;
+    }
+    
+    isPlaceable(position, piece) {
+        for (let point of piece.pointShape) {
+            if ((point.y + position.y) > this.board.length - 1
+                || (point.x + position.x - piece.offCenter > this.board[0].length - 1
+                || (point.y + position.y) < 0
+                || (point.x + position.x - piece.offCenter < 0
+                || this.board[point.y + position.y][point.x + position.x - piece.offCenter] != 0))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    placePiece(position, piece) {
+        for (let point of piece.pointShape) {
+            if (!point.isMiddle) {
+                this.board[point.y + position.y][point.x + position.x - piece.offCenter] = piece.id;
+            } else {
+                this.board[point.y + position.y][point.x + position.x - piece.offCenter] = piece.id + 16;
+            }
+        }
+    }
+
+    takeBackPiece(position, piece) {
+        for (let point of piece.pointShape) {
+            this.board[point.y + position.y][point.x + position.x - piece.offCenter] = 0;
+        }
+    }
+}
+
+export { LegionSolver };
---
+++ src/modules/piece.js
@@ -0,0 +1,97 @@
+import { Point } from './point.js';
+import _ from 'underscore';
+
+class Piece {
+    constructor(shape, amount, id) {
+        this.shape = shape;
+        this.amount = amount;
+        this.id = id;
+    }
+
+    get pointShape() {
+        Object.defineProperty(this, "pointShape", { value: []});
+
+        for (let i = 0; i < this.shape.length; ++i) {
+            for (let j = 0; j < this.shape[i].length; ++j) {
+                if (this.shape[i][j] == 1) {
+                    this.pointShape.push(new PiecePoint(j, i, false));
+                } else if (this.shape[i][j] == 2) {
+                    this.pointShape.push(new PiecePoint(j, i, true));    
+                }
+            }
+        }
+        return this.pointShape;
+    }
+
+    get offCenter() {
+        Object.defineProperty(this, "offCenter", { value: 0, writable: true });
+        
+        for (let i = 0; i < this.shape[0].length; i++) {
+            if (this.shape[0][i] == 1) {
+                this.offCenter = i;
+                break;
+            }
+        }
+        return this.offCenter;
+    }
+
+    get transformations() {
+        Object.defineProperty(this, "transformations", { value: []});
+
+        let shape = [...this.shape];
+        let newGrid;
+
+        for (let i = 0; i < 2; i++) {
+            for (let j = 0; j < 4; j++) {
+                newGrid = new Array(shape[0].length).fill(0).map(() => new Array(shape.length).fill(0));
+                for (let k = 0; k < shape.length; k++) {
+                    for (let l = 0; l < shape[0].length; l++) {
+                        if (shape[k][l] != 0) {
+                            newGrid[shape[0].length-l-1][k] = shape[k][l];
+                        }
+                    }
+                }
+                shape = newGrid;
+                this.transformations.push(new Piece(shape, this.amount, this.id));
+            }
+            newGrid = new Array(shape.length).fill(0).map(() => new Array(shape[0].length).fill(0));
+            for (let k = 0; k < shape.length; k++) {
+                for (let l = 0; l < shape[0].length; l++) {
+                    if (shape[k][l] != 0) {
+                        newGrid[shape.length-k-1][l] = shape[k][l];
+                    }
+                }
+            }
+            shape = newGrid;
+        }
+
+        for (let i = 0; i < this.transformations.length - 1; i++) {
+            for (let j = i + 1; j < this.transformations.length; j++) {
+                if (_.isEqual(this.transformations[i], this.transformations[j])) {
+                    this.transformations.splice(j,1);
+                    j--;
+                }
+            }
+        }
+
+        return this.transformations;
+    }
+
+    get pointTransformations() {
+        Object.defineProperty(this, "pointTransformations", { value: []});
+        for (let piece of this.transformations) {
+            this.pointTransformations.push(piece.pointShape);
+        }
+
+        return this.pointTransformations;
+    }
+}
+
+class PiecePoint extends Point {
+    constructor(x, y, isMiddle) {
+        super(x, y);
+        this.isMiddle = isMiddle;
+    }
+}
+
+export { Piece, PiecePoint };
