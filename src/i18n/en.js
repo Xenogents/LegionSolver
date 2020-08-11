1cf3818d2e99c6f54c68b7008d9c5349e4b1e6c1
luke2327 <qwepoi8526@gmail.com>
2020-08-11 11:31:47 +0900

    feature: i18n

---
+++ src/i18n/en.js
@@ -0,0 +1,54 @@
+export default {
+    title: 'Legion Solver',
+    instructions: 'Instructions:',
+    instructionsSub1: '1. Click the grid spaces you want to be filled, the region click will help you fill it in faster.',
+    instructionsSub2: '2. Input the amount of each shape you want to be filled in the board.',
+    instructionsSub3: '3. The space that the pieces take up should equal the amount of grid spaces you filled, although the program will still try to run otherwise.',
+    instructionsSub4: "4. When you press Start the program will try to fill the board spaces with the pieces you've chosen, click on Live Solve if you want to see the board filled in real time.",
+
+    spacesToBeFilled: 'Spaces to be Filled: ',
+    boardSpacesFilled: 'Board Spaces Filled: ',
+    iterations: 'Iterations: ',
+    time: 'Time: ',
+
+    bigClick: 'Region Click',
+    liveSolve: 'Live Solve',
+    darkMode: 'Dark Mode',
+
+    boardButton: 'Start',
+    resetButton: 'Reset',
+    clearPieces: 'Clear Pieces',
+    clearBoard: 'Clear Board',
+    failText: 'No Solution Found',
+
+    /** jobs */
+    // Lvl: 60
+    lvl60: 'Lvl 60',
+
+    // Lvl: 100
+    lvl100: 'Lvl 100',
+
+    // Lvl: 140
+    warriorPirate140: 'Lvl 140 Warrior/Pirate',
+    mageThiefArcher140: 'Lvl 140 Mage/Thief/Archer',
+
+    // Lvl: 200
+    warrior200: 'Lvl 200 Warrior',
+    archer200: 'Lvl 200 Archer',
+    thiefLab200: 'Lvl 200 Thief/Lab',
+    mage200: 'Lvl 200 Mage',
+    pirate200: 'Lvl 200 Pirate',
+
+    // Lvl: 250
+    warrior250: 'Lvl 250 Warrior',
+    archer250: 'Lvl 250 Archer',
+    thief250: 'Lvl 250 Thief',
+    mage250: 'Lvl 250 Mage',
+    pirate250: 'Lvl 250 Pirate',
+    xenon250: 'Lvl 250 Xenon',
+
+    /** special jobs */
+    enhancedLab200: 'Lvl 200 Enhanced Lab',
+    enhancedLab250: 'Lvl 250 Enhanced Lab',
+    lab250: 'Lvl 250 Lab'
+}
---
+++ src/i18n/index.js
@@ -0,0 +1,7 @@
+import en from './en';
+import ko from './ko';
+
+export default {
+  GMS: en,
+  KMS: ko
+}
