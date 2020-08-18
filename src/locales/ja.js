ec98bac793cfeea5e81cc1e501d2757510f560a1
luke2327 <qwepoi8526@gmail.com>
2020-08-18 13:49:39 +0900

    mod: code indentation

--- src/locales/ja.js
+++ src/locales/ja.js
@@ -1,57 +1,57 @@
 export default {
-  title: 'ユニオンマップメーカー',
-  instructions: '説明:',
+    title: 'ユニオンマップメーカー',
+    instructions: '説明:',
 
-  instructionsSub1: '1. ユニオンマップをもっと早く埋めるためには地域選択のボタンを活性化してから地域を選択してください。',
-  instructionsSub2: '2. ユニオンマップに埋めようとする職業の数を入力してください。',
-  instructionsSub3: '3. 算定された空間は選択された空間の量と同じでなければならず、同じでなければプログラムは繰り返します。',
-  instructionsSub4: '4. プログラムがルートを探しているときにその状況を見たいなら、実時間見るボタンを活性化してください。',
+    instructionsSub1: '1. ユニオンマップをもっと早く埋めるためには地域選択のボタンを活性化してから地域を選択してください。',
+    instructionsSub2: '2. ユニオンマップに埋めようとする職業の数を入力してください。',
+    instructionsSub3: '3. 算定された空間は選択された空間の量と同じでなければならず、同じでなければプログラムは繰り返します。',
+    instructionsSub4: '4. プログラムがルートを探しているときにその状況を見たいなら、実時間見るボタンを活性化してください。',
 
-  spacesToBeFilled: 'キャラクターの算定された空間: ',
-  boardSpacesFilled: 'マップの選択された空間: ',
-  iterations: '繰り返した数: ',
-  time: '所要時間: ',
+    spacesToBeFilled: 'キャラクターの算定された空間: ',
+    boardSpacesFilled: 'マップの選択された空間: ',
+    iterations: '繰り返した数: ',
+    time: '所要時間: ',
 
-  bigClick: '地域選択',
-  liveSolve: '実時間見る',
-  darkMode: 'ダークモード',
+    bigClick: '地域選択',
+    liveSolve: '実時間見る',
+    darkMode: 'ダークモード',
 
-  start: 'スタート',
-  pause: 'ポーズ',
-  continue: 'コンティニュー',
-  reset: 'リセット',
-  clearPieces: 'キャラクター初期化',
-  clearBoard: 'マップ初期化',
-  failText: 'ルートが見つかりませんでした',
+    start: 'スタート',
+    pause: 'ポーズ',
+    continue: 'コンティニュー',
+    reset: 'リセット',
+    clearPieces: 'キャラクター初期化',
+    clearBoard: 'マップ初期化',
+    failText: 'ルートが見つかりませんでした',
 
-  /** jobs */
-  // Lvl: 60
-  lvl60: 'Lvl 60',
+    /** jobs */
+    // Lvl: 60
+    lvl60: 'Lvl 60',
 
-  // Lvl: 100
-  lvl100: 'Lvl 100',
+    // Lvl: 100
+    lvl100: 'Lvl 100',
 
-  // Lvl: 140
-  warriorPirate140: 'Lvl 140 戦士/海賊',
-  mageThiefArcher140: 'Lvl 140 魔法使い/盗賊/弓',
+    // Lvl: 140
+    warriorPirate140: 'Lvl 140 戦士/海賊',
+    mageThiefArcher140: 'Lvl 140 魔法使い/盗賊/弓',
 
-  // Lvl: 200
-  warrior200: 'Lvl 200 戦士',
-  archer200: 'Lvl 200 弓',
-  thiefLab200: 'Lvl 200 盗賊/ゼノン',
-  mage200: 'Lvl 200 魔法使い',
-  pirate200: 'Lvl 200 海賊',
+    // Lvl: 200
+    warrior200: 'Lvl 200 戦士',
+    archer200: 'Lvl 200 弓',
+    thiefLab200: 'Lvl 200 盗賊/ゼノン',
+    mage200: 'Lvl 200 魔法使い',
+    pirate200: 'Lvl 200 海賊',
 
-  // Lvl: 250
-  warrior250: 'Lvl 250 戦士',
-  archer250: 'Lvl 250 弓',
-  thief250: 'Lvl 250 盗賊',
-  mage250: 'Lvl 250 魔法使い',
-  pirate250: 'Lvl 250 海賊',
-  xenon250: 'Lvl 250 ゼノン',
+    // Lvl: 250
+    warrior250: 'Lvl 250 戦士',
+    archer250: 'Lvl 250 弓',
+    thief250: 'Lvl 250 盗賊',
+    mage250: 'Lvl 250 魔法使い',
+    pirate250: 'Lvl 250 海賊',
+    xenon250: 'Lvl 250 ゼノン',
 
-  /** special jobs */
-  enhancedLab200: 'Lvl 200 Enhanced Lab',
-  enhancedLab250: 'Lvl 250 Enhanced Lab',
-  lab250: 'Lvl 250 Lab'
+    /** special jobs */
+    enhancedLab200: 'Lvl 200 Enhanced Lab',
+    enhancedLab250: 'Lvl 250 Enhanced Lab',
+    lab250: 'Lvl 250 Lab'
 }
