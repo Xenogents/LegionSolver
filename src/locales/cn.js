d246081d508502c97bf79cb73028c9b7f4874702
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2021-07-20 13:07:08 +0800

    Create cn.js

---
+++ src/locales/cn.js
@@ -0,0 +1,56 @@
+export default {
+    title: '冒险岛联盟自动排列工具',
+    instructions: '使用方法：',
+    instructionsSub1: '1. 点击你想要使用的区块，或者可以使用选取整个区块功能。',
+    instructionsSub2: '2. 输入你拥有的角色拼图数量。',
+    instructionsSub3: '3. 必须要确定可使用的区块数量和可填入的区块数量相等，否则自动排列程序将陷入死循环。',
+    instructionsSub4: "4. 只要点击开始，程序就会自动按照你输入的拼图数量填满区块。你也可以打开实时演算功能来查看过程。",
+
+    spacesToBeFilled: '可填入的区块数量: ',
+    boardSpacesFilled: '可使用的区块数量: ',
+    iterations: '尝试次数: ',
+    time: '消耗时间: ',
+
+    bigClick: '选取整个区块',
+    liveSolve: '实时演算',
+    darkMode: '暗黑模式',
+
+    start: '开始',
+    pause: '暂停',
+    continue: '继续',
+    reset: '重置',
+    clearPieces: '清除所有的拼图',
+    clearBoard: '清除面板',
+    failText: '找不到解',
+
+    /** jobs */
+    // 等级: 60
+    lvl60: '等级 60',
+
+    // 等级: 100
+    lvl100: '等级 100',
+
+    // 等级: 140
+    warriorPirate140: '等级 140 战士/海盗',
+    mageThiefArcher140: '等级 140 魔法师/飞侠/尖兵/弓箭手',
+
+    // 等级: 200
+    warrior200: '等级 200 战士',
+    archer200: '等级 200 弓箭手',
+    thiefLab200: '等级 200 飞侠/尖兵',
+    mage200: '等级 200 魔法师',
+    pirate200: '等级 200 海盗',
+
+    // 等级: 250
+    warrior250: '等级 250 战士',
+    archer250: '等级 250 弓箭手',
+    thief250: '等级 250 飞侠/尖兵',
+    mage250: '等级 250 魔法师',
+    pirate250: '等级 250 海盗',
+    xenon250: '等级 250 尖兵',
+
+    /** special jobs */
+    enhancedLab200: 'Lvl 200 Enhanced Lab',
+    enhancedLab250: 'Lvl 250 Enhanced Lab',
+    lab250: 'Lvl 250 Lab'
+}
