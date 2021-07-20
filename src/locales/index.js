0098f22682b805cebcbc7f215d609ff940ad6d3a
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2021-07-20 12:54:36 +0800

    Update index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,10 +2,12 @@ import en from './en.js';
 import ko from './ko.js';
 import ja from './ja.js';
 import tw from './tw.js';
+import cn from './cn.js';
 
 export default {
     GMS: en,
     KMS: ko,
     JMS: ja,
     TMS: tw,
+    CMS: cn,
 }
